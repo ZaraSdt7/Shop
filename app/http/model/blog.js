9c006e8121fad0010205bb71bb126b6b03f211c8
zahra <zahra.st7373@gmail.com>
2023-10-30 14:11:24 +0330

    blogmodel & productmodel

---
+++ app/http/model/blog.js
@@ -0,0 +1,42 @@
+const { default: mongoose } = require("mongoose");
+
+const Blogschema = new mongoose.Schema({
+author:{type:mongoose.Types.ObjectId , ref:"user",requried:true},
+title:{type:String ,requried:true},
+description:{type:String , requried:true},
+content:{type:String , requried:true}, //محتوا
+category:{type:[mongoose.Types.ObjectId], ref:"category"},
+comment:{type:[],default:[]},
+like:{type:[mongoose.Types.ObjectId] , default:[]},
+bookmark:{type:[mongoose.Types.ObjectId] , default:[]},
+readingTime:{type:Number , default:[]},
+similarblog:{type:mongoose.Types.ObjectId , default:[]}, //مقالات مشابه
+image:{type:String, default:[]},
+videoAddress:{type:String,default:[]}
+
+},{
+    timestamps:true,
+    toJSON:{
+        virtuals:true
+    }
+})
+Blogschema.virtual("user",{
+    ref:"user",
+    localField:"_id",
+    foreignField:"author"
+
+})
+Blogschema.virtual("category_detail",{
+    ref:"category",
+    localField:"_id",
+    foreignField:"category"
+})
+Blogschema.virtual("imageUrl").get(function(){
+    return this `${process.env.URL_BASE}:${process.env.PORT}/${this.image}`
+})
+Blogschema.virtual("videoUrl").get(function(){
+    return this `${process.env.URL_BASE}:${process.env.PORT}/${this.videoAddress}`
+})
+module.exports={
+    Blogmodel:mongoose.model("blog",Blogschema)
+}
---
+++ app/http/model/product.js
@@ -0,0 +1,40 @@
+const { default: mongoose } = require("mongoose");
+
+const Productschema = new mongoose.SchemaType({
+nema_product:{type:String , required:true},
+category:{type:mongoose.Types.ObjectId , ref:"category"},
+subcategory:{type :[String], required:true},  //نوع لباس
+description:{type:String , required: true},
+detail:{type:String , required:true},  //جزئیات محصول  
+image:{type:[String],required:true}, 
+color:{type:String , required:true},
+size:{type:String , required:true ,default:"free size"},
+size_detail:{type:Object, default:{
+    waist: "",
+    chest: "",
+    sleeve: "",
+    height: "",
+    width: ""    
+}},
+like:{type:[mongoose.Types.ObjectId], ref:"user",default:[]},
+dislike:{type:[mongoose.Types.ObjectId],ref:"user",default:[]},
+rate:{type:Number,default:[]},
+bookmark:{type:[mongoose.Types.ObjectId],ref:"user",default:[]},
+supplier:{type:mongoose.Types.ObjectId , ref:"user"},
+price:{type:Number, required:true,default:[] },
+discount:{type:Number , required:true , default:[]},
+count:{type:Number }
+
+},{
+   timestamps:true,
+   toJSON:{
+    virtuals:true
+   }
+})
+Productschema.index({name_product:"text",subcategory:"text",detail:"text"})
+Productschema.virtual("imageUrl").get(function(){
+    return this.image.map(img => `${process.env.URL_BASE}:${process.env.PORT}/${img}`)
+})
+module.exports ={
+    Productmodel:mongoose.model("product",Productschema)
+}
