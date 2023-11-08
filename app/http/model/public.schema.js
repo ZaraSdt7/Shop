f2538c49d1bb33d6f84925749c0dd332e068ca0e
zahra <zahra.st7373@gmail.com>
2023-11-08 23:10:25 +0330

    Add reply & RBAC in validation & models

--- app/http/model/public.schema.js
+++ app/http/model/public.schema.js
@@ -1,21 +1,23 @@
 const { default: mongoose } = require("mongoose");
 
-// const Answerschema = new mongoose.Schema({
-// userID:{type:mongoose.Types.ObjectId , ref:"user", required:true},
-// ProductID:{type:mongoose.Types.ObjectId, ref:"product", required:true},
-// comment:{type:String , required:true},
-// show:{type:Boolean , required:true , default:false},
-// opentocomment:{type:Boolean , default:false}    
-// },{
-//     timestamps:true,
-//     toJSON:{
-//         virtuals:true
-//     }
-// })
+const Replyschema = new mongoose.Schema({
+userID:{type:mongoose.Types.ObjectId , ref:"user", required:true},
+comment:{type:String , required:true},   
+},{
+    timestamps:true,
+    toJSON:{
+        virtuals:true
+    }
+})
 const Rateschema = new mongoose.Schema({
 ProductID:{type:mongoose.Types.ObjectId , ref:"product" , required:true},
 userID:{type:mongoose.Types.ObjectId , ref:"user", required:true},
 rating:{type:Number , default:0}    
+},{
+    timestamps:true,
+    toJSON:{
+        virtuals:true
+    }
 })
 const Commentschema = new mongoose.Schema({
 userID:{type:mongoose.Types.ObjectId , ref:"user", required:true},
@@ -23,7 +25,7 @@ ProductID:{type:mongoose.Types.ObjectId , ref:"product", required:true},
 comment:{type:String , required:true},
 show:{type:Boolean , required:true , default:false},
 opentocomment:{type:Boolean , default:false},
-reply:{type:[mongoose.Types.ObjectId],ref:"comment",default:[]}    
+reply:{type:[Replyschema],ref:"comment",default:[]}    
 },{
     timestamps:true
 
--- app/http/validations/user/Auth.schema.js
+++ app/http/validations/user/Auth.schema.js
@@ -1,4 +1,5 @@
 const joi = require("@hapi/joi");
+const createHttpError = require("http-errors");
 
 const Registerschema = joi.object({
 user_name:joi.string().required().length(30).error(new Error("Please enter a user_name")),
@@ -12,8 +13,14 @@ const Checkloginschema = joi.object({
 code:joi.string().min(4).max(6).error(new Error("The enter code is incorrect")),
 mobile:joi.string().length(11).pattern(/^09[0-9]{9}$/).required().error(new Error("Please enter a mobile"))    
 })
+ const Profileschema = joi.object({
+fullname:joi.string().error(createHttpError.BadRequest("please enter full name")),
+mobile:joi.string().length(11).pattern(/^09[0-9]{9}$/).required().error(new Error("Please enter a mobile")),
+address:joi.string().error(createHttpError.BadRequest("Please enter address"))     
+})
 module.exports={
     Registerschema,
     Loginschema,
-    Checkloginschema
+    Checkloginschema,
+    Profileschema
 }
